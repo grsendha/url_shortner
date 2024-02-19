93cd955595dbefb675b22893ee84a1728778d42c
grsendha <grsendha11@gmail.com>
2024-02-19 14:35:10 +0530

    jwt token added

--- controllers/user.js
+++ controllers/user.js
@@ -16,14 +16,15 @@ async function handleUserSignUp(req, res) {
 async function handleUserLogIn(req, res) {
   const { email, password } = req.body;
   const user = await User.findOne({ email, password });
+  console.log(user);
   if (!user) {
     return res.render("login", {
       error: "Invalid Username or Password",
     });
   }
-  const sessionId = uuidv4();
-  setUser(sessionId, user);
-  res.cookie("uid", sessionId);
+
+  const token = setUser(user);
+  res.cookie("uid", token);
   return res.redirect("/");
 }
 
--- services/auth.js
+++ services/auth.js
@@ -1,11 +1,23 @@
-const sessionIdToUserMap = new Map();
-
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+const jwt = require("jsonwebtoken");
+const secretKey = "asdfgh";
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secretKey
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  console.log(`token is ${token}`);
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secretKey);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
